d568f1a2eef80dc6eadcf4741a57eb1d6ef9aa37
t-boubacar <hassennagi@yahoo.fr>
2023-06-17 04:49:22 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main ()
+{
+	/**
+	 *string initialisation
+	 * */
+
+	char Mystr[] = "Programming is like building a multilingual puzzle\n";
+	puts(Mystr);
+
+	return 0;
+}	
