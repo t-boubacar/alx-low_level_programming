e025872d5149e8b61382ceda274973503e20cd06
t-boubacar <hassennagi@yahoo.fr>
2023-06-17 05:27:18 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * return: 0 always (Success)
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
