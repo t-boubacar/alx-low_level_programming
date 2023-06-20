2c229c84cee9b16a5ac5ac3dc364f211ae7aa0d4
t-boubacar <hassennagi@yahoo.fr>
2023-06-20 07:27:26 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/**
+ * _putchar - print characters
+ * main - main block
+ * return - On success
+ */
+int main ()
+{
+	char un = '_';
+	char p = 'p';
+	char u = 'u';
+	char t = 't';
+	char c = 'c';
+	char h = 'h';
+	char a = 'a';
+	char r = 'r';
+
+	putchar(un);
+	putchar(p);
+	putchar(u);
+	putchar(t);
+	putchar(c);
+	putchar(h);
+	putchar(a);
+	putchar(r);
+	return (0) ;
+}
+
