18e2ad51d5521b2fc0fc687a16fc7065b0d06475
t-boubacar <hassennagi@yahoo.fr>
2023-06-20 15:44:04 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,23 +6,7 @@
  */
 int main ()
 {
-	char un = '_';
-	char p = 'p';
-	char u = 'u';
-	char t = 't';
-	char c = 'c';
-	char h = 'h';
-	char a = 'a';
-	char r = 'r';
-
-	putchar(un);
-	putchar(p);
-	putchar(u);
-	putchar(t);
-	putchar(c);
-	putchar(h);
-	putchar(a);
-	putchar(r);
+	printf("_putchar\n");
 	return (0) ;
 }
 
