501936cc101dd51b8039953886ee760a7c102d79
t-boubacar <hassennagi@yahoo.fr>
2023-06-22 18:01:31 -0700

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,11 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
+
+#endif
